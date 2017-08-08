7d40d0badf09fd8341ba913f14b867e361e850b9
ngyuki <ngyuki.jp@gmail.com>
2017-08-08 21:56:38 +0900

    Fix test

--- tests/ActionRouteCollector.php
+++ tests/ActionRouteCollector.php
@@ -3,8 +3,23 @@ namespace ngyuki\RouteCollector\Tests;
 
 use ngyuki\RouteCollector\RouteCollector;
 
-/**
- * @method self controller(string $controller)
- * @method self action(string $action)
- */
-class ActionRouteCollector extends RouteCollector {}
+class ActionRouteCollector extends RouteCollector
+{
+    /**
+     * @param string $controller
+     * @return static
+     */
+    public function controller($controller)
+    {
+        return $this->param(__FUNCTION__, $controller);
+    }
+
+    /**
+     * @param string $action
+     * @return static
+     */
+    public function action($action)
+    {
+        return $this->param(__FUNCTION__, $action);
+    }
+}
